00320120cf7e4f4979085496d5a0268d786f4781
TobiasBanck <Tobiasbanck90@gmail.com>
2018-10-11 11:37:42 +0200

    Employee

--- src/assignment3/Employee.java
+++ src/assignment3/Employee.java
@@ -1,7 +1,51 @@
 package assignment3;
 
 public class Employee {
-    public static void main(String[] args) {
-        System.out.println("hej, testar GIT");
+    private String ID;
+    private String name;
+    private double grossSalary;
+    private final double TAX_RATE = 0*1;
+
+    public Employee(String ID, String name, double salary){
+        this.ID = ID;
+        this.name = name;
+        this.grossSalary = salary;
     }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setGrossSalary(double salary){
+        this.grossSalary = salary;
+    }
+
+    public String getID() {
+        return this.ID;
+    }
+
+    public String getName(){
+        return this.ID;
+    }
+
+    public double getGrossSalary(){
+        return this.grossSalary;
+    }
+    public double getNetSalary(){
+        return this.grossSalary - (grossSalary*TAX_RATE);
+    }
+
+    public boolean equals(Object object){
+        if (object instanceof  Employee){
+            Employee anotherEmployee = (Employee) object;
+            return this.ID.equals(anotherEmployee.getID());
+        }
+        return false;
+    }
+    public String toString(){
+        return this.name + " gross salary is of: " + grossSalary + " per month";
+    }
+
+
+
+
 }
